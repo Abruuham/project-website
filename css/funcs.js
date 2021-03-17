9ea53786976bfe661a8a06e9fec13b5525e64ed8
Abraham Calvillo <31970808+Abruuham@users.noreply.github.com>
2021-03-17 15:52:49 -0700

    adding login/sign up page and styling

--- css/funcs.js
+++ css/funcs.js
@@ -2,6 +2,7 @@ function DropDown(el) {
     this.dd = el;
     this.placeholder = this.dd.children('span');
     this.opts = this.dd.find('ul.dropdown > li');
+    this.value = this.dd.find('ul.dropdown > li > a')
     this.val = '';
     this.index = -1;
     this.initEvents();
@@ -13,13 +14,6 @@ DropDown.prototype = {
         obj.dd.on('click', function(event){
             $(this).toggleClass('active');
             return false;
-        });
-
-        obj.opts.on('click',function(){
-            var opt = $(this);
-            obj.val = opt.text();
-            obj.index = opt.index();
-            obj.placeholder.text(obj.val);
         });
     },
     getValue : function() {
