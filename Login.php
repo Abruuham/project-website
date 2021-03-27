746c3241581b268316412af8bba52dfda0ba93de
Abraham Calvillo <31970808+Abruuham@users.noreply.github.com>
2021-03-27 01:38:44 -0700

    Small fixes to sign up/in page

--- Login.php
+++ Login.php
@@ -18,7 +18,7 @@ $username = $password = "";
 $username_err = $password_err = $login_err = "";
  
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login']) ){
  
     // Check if username is empty
     if(empty(trim($_POST["username"]))){
@@ -84,10 +84,99 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
+
+
     
     // Close connection
     mysqli_close($link);
 }
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['signup']) ){
+  // Validate username
+  if(empty(trim($_POST["username"]))){
+    $username_err = "Please enter a username.";
+} else{
+    // Prepare a select statement
+    $sql = "SELECT id FROM users WHERE username = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_username);
+        
+        // Set parameters
+        $param_username = trim($_POST["username"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            /* store result */
+            mysqli_stmt_store_result($stmt);
+            
+            if(mysqli_stmt_num_rows($stmt) == 1){
+                $username_err = "This username is already taken.";
+            } else{
+                $username = trim($_POST["username"]);
+            }
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+}
+
+// Validate password
+if(empty(trim($_POST["password"]))){
+    $password_err = "Please enter a password.";     
+} elseif(strlen(trim($_POST["password"])) < 6){
+    $password_err = "Password must have atleast 6 characters.";
+} else{
+    $password = trim($_POST["password"]);
+}
+
+// // Validate confirm password
+// if(empty(trim($_POST["confirm_password"]))){
+//     $confirm_password_err = "Please confirm password.";     
+// } else{
+//     $confirm_password = trim($_POST["confirm_password"]);
+//     if(empty($password_err) && ($password != $confirm_password)){
+//         $confirm_password_err = "Password did not match.";
+//     }
+// }
+
+// Check input errors before inserting in database
+if(empty($username_err) && empty($password_err)){// && empty($confirm_password_err)){
+    
+    // Prepare an insert statement
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+     
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+        
+        // Set parameters
+        $param_username = $username;
+        $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            // Redirect to login page
+            header("location: login.php");
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+}
+
+// Close connection
+mysqli_close($link);
+
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -107,11 +196,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   <div class="wrapper fadeInDown">
     <div id="formContent">
       <!-- Tabs Titles -->
-      <h2 class="active noselect">Sign Up</h2>
-      <h2 class="inactive underlineHover noselect" id="change">Sign In</h2>
+      <h2 class="active noselect" id="change1">Sign Up</h2>
+      <h2 class="inactive underlineHover noselect" id="change2">Sign In</h2>
 
-      <!-- <a href=" login.php?p=su"><h2 class= <?= ((isset($_GET["id"])) && $_GET['p'] == 'su') ? "active noselect" : "inactive underlineHover noselect" ?>> Sign Up </h2></a>
-      <a href=" login.php?p=li"><h2 id="change" class= <?= ((isset($_GET["id"])) && $_GET['p'] == 'li') ? "active noselect" : "inactive underlineHover noselect" ?>> Log In </h2></a> -->
 
 
   
@@ -122,34 +209,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   
       <!-- Login Form -->
       <form id="form1" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <input type="text" id="signUp" class="fadeIn second" name="username"  placeholder="Sign Up" class ="form-control<?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+        <input type="text" id="signUp1" class="fadeIn second" name="username"  placeholder="Sign Up" class ="form-control<?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
 
 
         <span class="invalid-feedback"><?php echo $username_err; ?></span>
 
 
 
-        <input type="password" id="password" class="fadeIn third" name="password"  placeholder="password" class="form-control
+        <input type="password" id="password1" class="fadeIn third" name="password"  placeholder="password" class="form-control
         <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
 
         
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
-        <input type="submit" class="fadeIn fourth" value="Sign Up">
+        <input type="submit" id="btn1" class="fadeIn fourth" name="signup" value="Sign Up">
       </form>
       <form id="form2" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <input type="text" id="signUp" class="fadeIn second" name="username"  placeholder="Sign In" class ="form-control<?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+        <input type="text" id="signUp2" class="fadeIn second" name="username"  placeholder="Sign In" class ="form-control<?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
 
 
         <span class="invalid-feedback"><?php echo $username_err; ?></span>
 
 
 
-        <input type="password" id="password" class="fadeIn third" name="password"  placeholder="gfgsfgsfg" class="form-control
+        <input type="password" id="password2" class="fadeIn third" name="password"  placeholder="gfgsfgsfg" class="form-control
         <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
 
         
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
-        <input type="submit" class="fadeIn fourth" value="Log In">
+        <input type="submit"  id="btn2" class="fadeIn fourth" name = "login" value="Log In">
       </form>
 
 
--- css/forms.js
+++ css/forms.js
@@ -1,7 +1,43 @@
 $(function(){
-    $('#change').on('click',function(e){
+    $('#change2').on('click',function(e){
         e.preventDefault();
-        $('#form1').css('display','none', 'inactive');
-        $('#form2').css('display','block', 'active'); 
+        $('#signUp2').removeClass('fadeIn second');  
+        $('#password2').removeClass('fadeIn third');
+        $('#btn2').removeClass('fadeIn fourth');
+        
+        $('#form1').css('display','none');
+        $('#change1').removeClass('active');
+        $('#change1').addClass('inactive');
+        $('#change1').addClass('underlineHover');
+
+        $('#form2').css('display','block');
+        $('#change2').removeClass('inactive');
+        $('#change2').addClass('active');  
+        $('#change2').removeClass('underlineHover');  
+
+        
+
+    });
+});
+$(function(){
+    $('#change1').on('click',function(e){
+        e.preventDefault();
+
+        $('#signUp1').removeClass('fadeIn second');  
+        $('#password1').removeClass('fadeIn third');
+        $('#btn1').removeClass('fadeIn fourth');
+
+        $('#form1').css('display','block');
+        $('#change1').removeClass('inactive');
+        $('#change1').addClass('active');
+        $('#change1').removeClass('underlineHover');  
+
+        
+        $('#form2').css('display','none');
+        $('#change2').removeClass('active');
+        $('#change2').addClass('inactive'); 
+        $('#change2').addClass('underlineHover');  
+
+        
     });
 });
